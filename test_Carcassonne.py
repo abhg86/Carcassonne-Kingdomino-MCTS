e1422211340a8b0c27c2ef5393a9fe96796b66bd
AymericB <aymeric.behaegel@ens-rennes.fr>
2025-04-04 17:57:17 +0200

    Beginning of AlphaZero algo

--- test_Carcassonne.py
+++ test_Carcassonne.py
@@ -10,22 +10,21 @@ from wingedsheep.carcassonne.objects.actions.pass_action import PassAction
 from wingedsheep.carcassonne.tile_sets.supplementary_rules import SupplementaryRule  
 from wingedsheep.carcassonne.tile_sets.tile_sets import TileSet  
    
-game = CarcassonneGame(  
-	players=2,  
-	tile_sets=[TileSet.BASE],  
-	supplementary_rules=[]  
-)  
   
 start = time.time()
-while not game.is_finished():  
-    player: int = game.get_current_player()  
-    valid_actions: [Action] = game.get_possible_actions()  
-    action: Optional[Action] = random.choice(valid_actions)  
-    if type(action) == MeepleAction:
-        print(action.coordinate_with_side.coordinate.row, action.coordinate_with_side.coordinate.column)
+for i in range(70):
+    game = CarcassonneGame(  
+        players=2,  
+        tile_sets=[TileSet.BASE],  
+        supplementary_rules=[]  
+    )  
+    while not game.is_finished():  
+        player: int = game.get_current_player()  
+        valid_actions: [Action] = game.get_possible_actions(nb_max=1,rdm=True)  
+        action: Optional[Action] = random.choice(valid_actions)
 
-    if action is not None:  
-        game.step(player, action)  
-    game.render() 
+        if action is not None:  
+            game.step(player, action)  
+        # game.render()
 
 print(time.time() - start)
