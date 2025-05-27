a21c2cf508f83069c1452bd1980876dab26dc4d9
AymericB <aymeric.behaegel@ens-rennes.fr>
2025-05-27 11:16:38 +0200

    heuristic score MCTS

--- Compare.py
+++ Compare.py
@@ -2,6 +2,7 @@ import logging
 from random import choice
 import numpy as np
 from A0_MCTS import A0_MCTS
+from heuristic_score_MCTS import Sc_MCTS
 import torch
 
 from A0_NNet import NNetWrapper
@@ -9,7 +10,7 @@ from Arena import Arena
 from MCTS_util import dotdict, number_to_action
 from wingedsheep.carcassonne.utils.action_util import ActionUtil
 
-log = logging.getLogger(__name__)
+# log = logging.getLogger(__name__)
 
 BOARD_SIZE = 35
 
@@ -19,7 +20,7 @@ args = dotdict({
     'tempThreshold': 15,        #
     'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
-    'numMCTSSims': 2,          # Number of games moves for MCTS to simulate.
+    'numMCTSSims': 72,          # Number of games moves for MCTS to simulate.
     'arenaCompare': 2,         # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 1,
 
@@ -39,13 +40,14 @@ args_net = dotdict({
     'num_channels': 1,
 })
 
-nnet = NNetWrapper(args_net, BOARD_SIZE, player_nb=2)
-nnet.load_checkpoint(folder=args.checkpoint, filename='temp.pth.tar')
-mcts = A0_MCTS(nnet, args.numMCTSSims, args.cpuct,board_size=BOARD_SIZE)
+# nnet = NNetWrapper(args_net, BOARD_SIZE, player_nb=2)
+# nnet.load_checkpoint(folder=args.checkpoint, filename='temp.pth.tar')
+# mcts = A0_MCTS(nnet, args.numMCTSSims, args.cpuct,board_size=BOARD_SIZE)
+mcts = Sc_MCTS(args.numMCTSSims, args.cpuct,board_size=BOARD_SIZE)
 
 
-log.info('PITTING AGAINST PREVIOUS VERSION')
-arena = Arena(lambda x: number_to_action(np.argmax(mcts.getActionProb(x, temp=0)), x.phase, x.next_tile, board_size=BOARD_SIZE),
+# log.info('PITTING AGAINST PREVIOUS VERSION')
+arena = Arena(lambda x: number_to_action(np.argmax(mcts.getActionProb(x, temp=0, num_plays=3)), x.phase, x.next_tile, board_size=BOARD_SIZE),
                 lambda x: choice(ActionUtil.get_possible_actions(x)))
 wins1, wins2, draws = arena.playGames(args.arenaCompare)
 
---
+++ heuristic_score_MCTS.py
@@ -0,0 +1,166 @@
+import logging
+import math
+import random
+
+import numpy as np
+from wingedsheep.carcassonne.carcassonne_game_state import CarcassonneGameState
+from wingedsheep.carcassonne.objects.actions.action import Action
+from wingedsheep.carcassonne.objects.actions.meeple_action import MeepleAction
+from wingedsheep.carcassonne.objects.actions.pass_action import PassAction
+from wingedsheep.carcassonne.objects.actions.tile_action import TileAction
+from wingedsheep.carcassonne.objects.coordinate import Coordinate
+from wingedsheep.carcassonne.objects.coordinate_with_side import CoordinateWithSide
+from wingedsheep.carcassonne.objects.game_phase import GamePhase
+from wingedsheep.carcassonne.objects.meeple_type import MeepleType
+from wingedsheep.carcassonne.objects.side import Side
+from wingedsheep.carcassonne.objects.tile import Tile
+from wingedsheep.carcassonne.utils.action_util import ActionUtil
+from wingedsheep.carcassonne.utils.state_updater import StateUpdater
+from MCTS_util import *
+
+EPS = 1e-8
+BOARD_SIZE = 35
+
+log = logging.getLogger(__name__)
+
+
+class Sc_MCTS():
+    """
+    This class handles the MCTS tree.
+    """
+
+    def __init__(self, numMCTSSims, cpuct, board_size=BOARD_SIZE):
+        self.numMCTSSims = numMCTSSims
+        self.cpuct = cpuct
+        self.Wsa = {}  # stores number of wins for s,a
+        self.Nsa = {}  # stores #times edge s,a was visited
+        self.Ns = {}  # stores #times board s was visited
+
+        self.Vs = {}  # stores game.getValidMoves for board s
+        
+        self.ActionSize = board_size*board_size*9 +1
+        
+    def refresh_tree(self):
+        """
+        This function clears the MCTS tree.
+        """
+        self.Wsa.clear()
+        self.Nsa.clear()
+        self.Ns.clear()
+        self.Vs.clear()
+
+    def heuristic_playout(self, state:CarcassonneGameState, num_plays:int=10):
+        """
+        This function performs a playout for num_plays iterations.
+        It returns the win score of the player in the state it ends up in.
+        """
+        state = state.copy()
+        for _ in range(num_plays):
+            if state.is_terminated():
+                break
+            valid_actions: [Action] = ActionUtil.get_possible_actions(state,nb_max=1,rdm=True)  
+            action = valid_actions[0]       # already randomly chosen
+            if action is not None:  
+                state = StateUpdater.apply_action(game_state=state, action=action)
+        wins = score_to_win(state.scores, True)           #-1 and 1 here
+        res = np.zeros(len(wins))                   # 0 and 1 needed for regular MCTS
+        if np.max(wins) == 0.1:             # 0.1 is the score for a draw
+            res = np.ones(len(wins)) / len(wins)
+        else:
+            res[np.argmax(wins)] = 1
+        return res
+    
+    def getActionProb(self, state, temp=1, num_plays:int=10):
+        """
+        This function performs numMCTSSims simulations of MCTS starting from
+        state.
+
+        Returns:
+            probs: a policy vector where the probability of the ith action is
+                   proportional to Nsa[(s,a)]**(1./temp)
+        """
+        self.refresh_tree()
+        for i in range(self.numMCTSSims):
+            state = state.copy()
+            self.search(state, num_plays=num_plays)
+
+        s = to_hash(state)
+        counts = [self.Nsa[(s, a)] if (s, a) in self.Nsa else 0 for a in range(self.ActionSize)]
+
+        if temp == 0:
+            bestAs = np.array(np.argwhere(counts == np.max(counts))).flatten()
+            bestA = np.random.choice(bestAs)
+            probs = [0] * len(counts)
+            probs[bestA] = 1
+            return probs
+
+        counts = [x ** (1. / temp) for x in counts]
+        counts_sum = float(sum(counts))
+        probs = [x / counts_sum for x in counts]
+        return probs
+
+    def search(self, state, num_plays:int=10):
+        """
+        This function performs one iteration of MCTS. It is recursively called
+        till a leaf node is found. The action chosen at each node is one that
+        has the maximum upper confidence bound as in the paper.
+
+        Once a leaf node is found, the neural network is called to return an
+        initial policy P and a value v for the state. This value is propagated
+        up the search path. In case the leaf node is a terminal state, the
+        outcome is propagated up the search path. The values of Ns, Nsa, Qsa are
+        updated.
+
+
+        Returns:
+            v: the value of the current state
+        """
+
+        s = to_hash(state)
+
+        if state.is_terminated():
+            # terminal node
+            return score_to_win(state.scores, True)
+
+        if s not in self.Ns:
+            # leaf node
+            self.Ns[s] = 0
+            self.Vs[s] = ActionUtil.getValidMovesMask(state,self.ActionSize,BOARD_SIZE)  # get the valid moves
+            return self.heuristic_playout(state, num_plays=10)
+
+        valids = self.Vs[s]
+        cur_best = -float('inf')
+        best_act = -1
+
+        # pick the action with the highest upper confidence bound
+        for a in range(self.ActionSize):
+            if valids[a]:
+                if (s, a) in self.Nsa:
+                    Q = self.Wsa[(s, a)] / self.Nsa[(s, a)]
+                    
+                    u = Q + 0.4  * math.sqrt(math.log(self.Ns[s]) / (self.Nsa[(s, a)]))
+                else:
+                    u = float('inf')
+
+                if u > cur_best:
+                    cur_best = u
+                    best_act = a
+
+        a = best_act
+        action = number_to_action(a,state.phase, state.next_tile)
+        next_s = StateUpdater.apply_action(game_state=state, action=action)     # deep copy already made within function
+
+        next_s.deck.append(next_s.next_tile)                                    # acquiring random new next_tile 
+        next_s.next_tile = random.choice(next_s.deck)
+        next_s.deck.remove(next_s.next_tile)
+
+        v = self.search(next_s, num_plays=num_plays)
+
+        self.Ns[s] += 1
+        if (s, a) not in self.Nsa:
+            self.Nsa[(s, a)] = 1
+            self.Wsa[(s, a)] = v[state.current_player]
+        else:
+            self.Nsa[(s, a)] += 1
+            self.Wsa[(s, a)] += v[state.current_player]  # the player who played the action
+        return v
