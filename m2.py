09a088938d23ed5d041fedcc3b6dc6d24cd20569
AymericB <aymeric.behaegel@ens-rennes.fr>
2025-07-16 11:32:58 +0200

    Modif to check set_start_method

--- m2.py
+++ m2.py
@@ -2,5 +2,5 @@ from multiprocessing import set_start_method
 from m1 import f3
 
 if __name__ == '__main__':
-    # set_start_method('spawn', force=True)  # Required for multiprocessing on some platforms
+    set_start_method('spawn', force=True)  # Required for multiprocessing on some platforms
     print(f3()) # this would run multiprocessing code
